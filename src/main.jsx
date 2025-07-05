389151edbe7b9a0e00a8ac283d7a4827d01b9930
Harsheed <74187847+HarsheedRabhadia@users.noreply.github.com>
2025-07-05 16:36:47 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx"; // Ensure this path is correct
-import "./index.css"; // Ensure this imports your Tailwind CSS file
+import App from "./App.jsx";
+import "./index.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
